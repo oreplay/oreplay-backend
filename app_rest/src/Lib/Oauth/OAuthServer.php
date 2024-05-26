6d2a7b6131325872aa6e76db7f8f17703d0fbd45
Adrian <freefri@freefri.es>
2024-05-26 17:29:59 +0200

    COMPOSER UPDATE

--- app_rest/src/Lib/Oauth/OAuthServer.php
+++ app_rest/src/Lib/Oauth/OAuthServer.php
@@ -4,6 +4,7 @@ declare(strict_types = 1);
 
 namespace App\Lib\Oauth;
 
+use App\Controller\ApiController;
 use RestApi\Lib\Helpers\OauthBaseServer;
 use RestApi\Model\Table\OauthAccessTokensTable;
 
@@ -12,5 +13,10 @@ class OAuthServer extends OauthBaseServer
     protected function loadStorage(): OauthAccessTokensTable
     {
         return \App\Model\Table\OauthAccessTokensTable::load();
+    }
+
+    protected function silentVerificationPath(): string
+    {
+        return ApiController::ROUTE_PREFIX . '/me';
     }
 }
