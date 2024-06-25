3c6f12213b4b076f6808155b709c55af71cb23e3
Sergio G. Pajares <sergiogarciapajares@gmail.com>
2024-06-25 19:28:24 +0200

    ADDED events ?when utests

--- app_rest/plugins/Results/tests/Fixture/EventsFixture.php
+++ app_rest/plugins/Results/tests/Fixture/EventsFixture.php
@@ -4,6 +4,7 @@ declare(strict_types = 1);
 
 namespace Results\Test\Fixture;
 
+use DateTime;
 use RestApi\TestSuite\Fixture\RestApiFixture;
 use Results\Model\Entity\Event;
 use Results\Model\Entity\Federation;
@@ -14,26 +15,53 @@ class EventsFixture extends RestApiFixture
 
     public const FIRST_RAID = '1b10cfcc-b3f2-40bb-8dbe-8cb5d8b24c00';
 
-    public $records = [
-        [
-            'id' => Event::FIRST_EVENT,
-            'description' => 'Test Foot-o',
-            'initial_date' => '2024-01-25',
-            'final_date' => '2024-01-25',
-            'federation_id' => Federation::FEDO,
-            'created' => '2022-03-01 10:01:00',
-            'modified' => '2022-03-01 10:01:00',
-            'deleted' => null,
-        ],
-        [
-            'id' => EventsFixture::FIRST_RAID,
-            'description' => 'Test Adventure Race',
-            'initial_date' => '2024-01-26',
-            'final_date' => '2024-01-26',
+    public function init (): void
+    {
+        $this->records = [
+            [
+                'id' => Event::FIRST_EVENT,
+                'description' => 'Test Foot-o',
+                'initial_date' => '2024-01-25',
+                'final_date' => '2024-01-25',
+                'federation_id' => Federation::FEDO,
+                'created' => '2022-03-01 10:01:00',
+                'modified' => '2022-03-01 10:01:00',
+                'deleted' => null,
+            ],
+            [
+                'id' => EventsFixture::FIRST_RAID,
+                'description' => 'Test Adventure Race',
+                'initial_date' => '2024-01-26',
+                'final_date' => '2024-01-26',
+                'federation_id' => Federation::IOF,
+                'created' => '2022-03-07 10:01:00',
+                'modified' => '2022-03-07 10:01:00',
+                'deleted' => null,
+            ],
+            [
+                'id' => '1b10cfcc-b3f2-40bb-8dbe-8b24c0-today',
+                'description' => 'Today event',
+                'initial_date' => (new DateTime('now'))->format('Y-m-d'),
+                'final_date' => (new DateTime('now'))->format('Y-m-d'),
+                'federation_id' => Federation::IOF,
+                'created' => '2022-03-10 10:01:00',
+                'modified' => '2022-03-10 10:01:00',
+                'deleted' => null,
+            ],
+            [
+            'id' => '1b10cfcc-b3f2-40bb-8dbe-8b2-tomorrow',
+            'description' => 'Tomorrow event',
+            'initial_date' => (new DateTime('now'))->modify("+1 day")->format('Y-m-d'),
+            'final_date' => (new DateTime('now'))->modify("+1 day")->format('Y-m-d'),
             'federation_id' => Federation::IOF,
-            'created' => '2022-03-07 10:01:00',
-            'modified' => '2022-03-07 10:01:00',
+            'created' => '2022-03-13 10:01:00',
+            'modified' => '2022-03-13 10:01:00',
             'deleted' => null,
-        ],
-    ];
+        ]
+        ];
+
+        parent::init();
+    }
+
+
 }
