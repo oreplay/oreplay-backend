23b73059943f2c770a045c63597f936b0e3a6c9c
Adrian <freefri@freefri.es>
2024-10-24 18:11:12 +0200

    #109 KVjfDvVu (to revert) quickfix problem with auth and servers timezone, do not check expiration time of codes

--- app_rest/src/Model/Table/OauthAccessTokensTable.php
+++ app_rest/src/Model/Table/OauthAccessTokensTable.php
@@ -6,4 +6,22 @@ namespace App\Model\Table;
 
 class OauthAccessTokensTable extends \RestApi\Model\Table\OauthAccessTokensTable
 {
+    public function getAuthorizationCode($code)
+    {
+        $code = $this->_findAuthorizationCodes($code);
+        if (!$code) {
+            return false;
+        }
+        $expires = $code->expires;
+        if ($expires->isPast()) {
+            //return false;// TODO
+        }
+        $code = $code->toArray();
+        $code['expires'] = $expires->getTimestamp();
+        return $code;
+    }
+    private function _findAuthorizationCodes($code)
+    {
+        return $this->OauthAuthorizationCodes->find()->where(['authorization_code' => $code])->firstOrFail();
+    }
 }
