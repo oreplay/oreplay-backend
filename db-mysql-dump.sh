c5777a32e355b88c8f6cf8fda9cdc58aeabbcf20
Adrian <freefri@freefri.es>
2024-12-02 18:36:03 +0100

    properly extract db connection from ENV

--- db-mysql-dump.sh
+++ db-mysql-dump.sh
@@ -7,12 +7,12 @@ if [ -z "$DATABASE_URL" ]; then
 fi
 
 # Extract MySQL credentials from DATABASE_URL
-DB_PROTOCOL=$(echo $DATABASE_URL | awk -F '://' '{print $1}')
-DB_USER=$(echo $DATABASE_URL | awk -F '[:@]' '{print $2}')
-DB_PASSWORD=$(echo $DATABASE_URL | awk -F '[:@]' '{print $3}')
-DB_HOST=$(echo $DATABASE_URL | awk -F '[@:]' '{print $4}')
-DB_PORT=$(echo $DATABASE_URL | awk -F '[:/]' '{print $5}')
-DB_NAME=$(echo $DATABASE_URL | awk -F '/' '{print $4}')
+DB_PROTOCOL=$(echo "$DATABASE_URL" | sed -n 's#^\(.*\)://.*#\1#p')
+DB_USER=$(echo "$DATABASE_URL" | sed -n 's#^[^:]*://\([^:]*\):.*@\([^:/]*\).*#\1#p')
+DB_PASSWORD=$(echo "$DATABASE_URL" | sed -n 's#^[^:]*://[^:]*:\([^@]*\)@.*#\1#p')
+DB_HOST=$(echo "$DATABASE_URL" | sed -n 's#^[^:]*://[^@]*@\(.*\):[0-9]*/.*#\1#p')
+DB_PORT=$(echo "$DATABASE_URL" | sed -n 's#.*://.*@.*:\([0-9]*\)/.*#\1#p')
+DB_NAME=$(echo "$DATABASE_URL" | sed -n 's#.*/\([^/?]*\).*#\1#p')
 
 # Ensure the protocol is MySQL
 if [ "$DB_PROTOCOL" != "mysql" ]; then
@@ -21,14 +21,16 @@ if [ "$DB_PROTOCOL" != "mysql" ]; then
 fi
 
 # Create the dump file
-DUMP_FILE="mysql_dump_$(date +%Y%m%d%H%M%S).sql"
+DUMP_FILE="mysql_dump_oreplay_$(date +%Y%m%d%H%M%S).sql"
 
-echo "Creating dump for database '$DB_NAME' on host '$DB_HOST:$DB_PORT'..."
+echo "Creating dump for database '$DB_NAME' on host '$DB_HOST:$DB_PORT' using user '$DB_USER' ..."
 mysqldump -u "$DB_USER" -p"$DB_PASSWORD" -h "$DB_HOST" -P "$DB_PORT" "$DB_NAME" > "$DUMP_FILE"
 
 # Check if the dump was successful
 if [ $? -eq 0 ]; then
     echo "Database dump created successfully: $DUMP_FILE"
+    echo "Please, immediately download the file and remove it from the server"
+    echo "kubectl cp oreplay/cakeapi-nginx-deployment-7cb85d7f75-rg5t8:$DUMP_FILE ./$DUMP_FILE"
 else
     echo "Error: Failed to create database dump."
     exit 1
