a1c269206f8b5e33a4baa87766fdad9bbf2f045a
Adrian <freefri@freefri.es>
2025-06-26 19:39:14 +0200

    add new example with duplicated bibs that should save the runner anyways

--- app_rest/plugins/Results/tests/TestCase/Controller/UploadExamples/IntermediateExamples.php
+++ app_rest/plugins/Results/tests/TestCase/Controller/UploadExamples/IntermediateExamples.php
@@ -188,4 +188,241 @@ class IntermediateExamples
             ]
         ];
     }
+
+    public static function itermediateWithDuplicatedBibs()
+    {
+        return [
+            'configuration' => [
+                'file' => 'D:zz_resulRadio.xml',
+                'extension' => 'XML',
+                'utf' => true,
+                'known_data' => true,
+                'contents' => 'ResultList',
+                'results_type' => 'Radiocontrols',
+                'one_stage' => true,
+                'source' => 'OEv12',
+                'iof_version' => '3.0',
+                'include_score' => false,
+                'trailo_type' => 'Other',
+                'trailo_at' => 'Other',
+                'trailo_normal' => '0',
+                'trailo_group' => '0',
+                'totalization' => 'Other'
+            ],
+            'event' => [
+                'id' => Event::FIRST_EVENT,
+                'description' => 'Championship',
+                'is_hidden' => false,
+                'stages' => [
+                    (int) 0 => [
+                        'id' => StagesFixture::STAGE_FEDO_2,
+                        'description' => 'Long, Forest',
+                        'controls' => [
+                            (int) 0 => [
+                                'station' => '165'
+                            ],
+                            (int) 1 => [
+                                'station' => '158'
+                            ],
+                            (int) 2 => [
+                                'station' => '136'
+                            ],
+                            (int) 3 => [
+                                'station' => '137'
+                            ]
+                        ],
+                        'classes' => [
+                            (int) 0 => [
+                                'id' => '',
+                                'uuid' => '',
+                                'oe_key' => '1',
+                                'short_name' => 'INF FEM',
+                                'long_name' => 'INFANTIL FEMENINO',
+                                'teams' => [],
+                                'course' => [
+                                    'id' => '',
+                                    'uuid' => '',
+                                    'distance' => '3850.0',
+                                    'climb' => '160.0',
+                                    'controls' => (int) 14,
+                                    'oe_key' => '9002',
+                                    'short_name' => 'F - INFANTIL'
+                                ],
+                                'classes_controls' => [
+                                    (int) 0 => [
+                                        'control' => [
+                                            'station' => '165'
+                                        ]
+                                    ],
+                                    (int) 1 => [
+                                        'control' => [
+                                            'station' => '158'
+                                        ]
+                                    ]
+                                ],
+                                'runners' => [
+                                    (int) 0 => [
+                                        'id' => '',
+                                        'uuid' => '',
+                                        'sicard' => '8502422',
+                                        'sex' => 'F',
+                                        'first_name' => 'Sara',
+                                        'last_name' => 'Alonso',
+                                        'db_id' => '19808',
+                                        'bib_number' => '1',
+                                        'sicard_alt' => '',
+                                        'is_nc' => false,
+                                        'runner_results' => [
+                                            (int) 0 => [
+                                                'id' => '',
+                                                'start_time' => '2025-06-20T09:40:00.000+02:00',
+                                                'status_code' => '0',
+                                                'time_neutralization' => (int) 0,
+                                                'time_adjusted' => (int) 0,
+                                                'time_penalty' => (int) 0,
+                                                'time_bonus' => (int) 0,
+                                                'leg_number' => (int) 1,
+                                                'splits' => [
+                                                    (int) 0 => [
+                                                        'sicard' => '8502422',
+                                                        'station' => '165',
+                                                        'points' => (int) 0,
+                                                        'bib_runner' => '1',
+                                                        'order_number' => (int) 1
+                                                    ],
+                                                    (int) 1 => [
+                                                        'sicard' => '8502422',
+                                                        'station' => '158',
+                                                        'points' => (int) 0,
+                                                        'bib_runner' => '1',
+                                                        'order_number' => (int) 2
+                                                    ]
+                                                ],
+                                                'result_type' => [
+                                                    'id' => 'e4ddfa9d-3347-47e4-9d32-c6c119aeac0e',
+                                                    'description' => 'Stage'
+                                                ]
+                                            ]
+                                        ],
+                                        'club' => [
+                                            'id' => '',
+                                            'uuid' => '',
+                                            'oe_key' => '10',
+                                            'short_name' => 'ARAGÓN',
+                                            'long_name' => 'ARAGÓN'
+                                        ]
+                                    ],
+                                    (int) 1 => [
+                                        'id' => '',
+                                        'uuid' => '',
+                                        'sicard' => '8525829',
+                                        'sex' => 'F',
+                                        'first_name' => 'Sonia',
+                                        'last_name' => 'Estibaliz',
+                                        'db_id' => '14844',
+                                        'bib_number' => '1',
+                                        'sicard_alt' => '',
+                                        'is_nc' => false,
+                                        'runner_results' => [
+                                            (int) 0 => [
+                                                'id' => '',
+                                                'start_time' => '2025-06-20T09:43:00.000+02:00',
+                                                'status_code' => '0',
+                                                'time_neutralization' => (int) 0,
+                                                'time_adjusted' => (int) 0,
+                                                'time_penalty' => (int) 0,
+                                                'time_bonus' => (int) 0,
+                                                'leg_number' => (int) 1,
+                                                'splits' => [
+                                                    (int) 0 => [
+                                                        'sicard' => '8525829',
+                                                        'station' => '165',
+                                                        'points' => (int) 0,
+                                                        'reading_time' => '2025-06-20T10:13:48.000+02:00',
+                                                        'reading_milli' => (int) 1750407228000,
+                                                        'time_seconds' => (int) 1848,
+                                                        'bib_runner' => '1',
+                                                        'order_number' => (int) 1
+                                                    ],
+                                                    (int) 1 => [
+                                                        'sicard' => '8525829',
+                                                        'station' => '158',
+                                                        'points' => (int) 0,
+                                                        'bib_runner' => '1',
+                                                        'order_number' => (int) 2
+                                                    ]
+                                                ],
+                                                'result_type' => [
+                                                    'id' => 'e4ddfa9d-3347-47e4-9d32-c6c119aeac0e',
+                                                    'description' => 'Stage'
+                                                ]
+                                            ]
+                                        ],
+                                        'club' => [
+                                            'id' => '',
+                                            'uuid' => '',
+                                            'oe_key' => '17',
+                                            'short_name' => 'CASTILLA Y LEÓN',
+                                            'long_name' => 'CASTILLA Y LEÓN'
+                                        ]
+                                    ],
+                                    (int) 2 => [
+                                        'id' => '',
+                                        'uuid' => '',
+                                        'sicard' => '8655840',
+                                        'sex' => 'F',
+                                        'first_name' => 'Elena',
+                                        'last_name' => 'Rodriguez',
+                                        'db_id' => '31758',
+                                        'bib_number' => '1',
+                                        'sicard_alt' => '',
+                                        'is_nc' => false,
+                                        'runner_results' => [
+                                            (int) 0 => [
+                                                'id' => '',
+                                                'start_time' => '2025-06-20T09:37:00.000+02:00',
+                                                'status_code' => '0',
+                                                'time_neutralization' => (int) 0,
+                                                'time_adjusted' => (int) 0,
+                                                'time_penalty' => (int) 0,
+                                                'time_bonus' => (int) 0,
+                                                'leg_number' => (int) 1,
+                                                'splits' => [
+                                                    (int) 0 => [
+                                                        'sicard' => '8655840',
+                                                        'station' => '165',
+                                                        'points' => (int) 0,
+                                                        'bib_runner' => '1',
+                                                        'order_number' => (int) 1
+                                                    ],
+                                                    (int) 1 => [
+                                                        'sicard' => '8655840',
+                                                        'station' => '158',
+                                                        'points' => (int) 0,
+                                                        'bib_runner' => '1',
+                                                        'order_number' => (int) 2
+                                                    ]
+                                                ],
+                                                'result_type' => [
+                                                    'id' => 'e4ddfa9d-3347-47e4-9d32-c6c119aeac0e',
+                                                    'description' => 'Stage'
+                                                ]
+                                            ]
+                                        ],
+                                        'club' => [
+                                            'id' => '',
+                                            'uuid' => '',
+                                            'oe_key' => '7',
+                                            'short_name' => 'LA RIOJA',
+                                            'long_name' => 'LA RIOJA'
+                                        ]
+                                    ]
+                                ]
+                            ]
+                        ]
+                    ]
+                ]
+            ]
+        ];
+    }
 }
