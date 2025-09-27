656165cd45d712e64eacfeecbabddc87849dc290
Adrian <freefri@freefri.es>
2025-09-27 21:33:54 +0200

    v0.3.7

--- app_rest/src/Controller/SwaggerJsonController.php
+++ app_rest/src/Controller/SwaggerJsonController.php
@@ -10,7 +10,7 @@ class SwaggerJsonController extends \RestApi\Controller\SwaggerJsonController
 {
     public static function version(): string
     {
-        return '0.3.6';
+        return '0.3.7';
     }
 
     protected function getContent(SwaggerReader $reader, array $paths): array
