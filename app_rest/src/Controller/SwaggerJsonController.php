982df6e3c1d1cc923d07940f5dc2e044ceba1c32
Adrian <freefri@freefri.es>
2025-10-24 08:04:30 +0200

    v0.4.1 (totals and relays in beta)

--- app_rest/src/Controller/SwaggerJsonController.php
+++ app_rest/src/Controller/SwaggerJsonController.php
@@ -10,7 +10,7 @@ class SwaggerJsonController extends \RestApi\Controller\SwaggerJsonController
 {
     public static function version(): string
     {
-        return '0.3.9';
+        return '0.4.1';
     }
 
     protected function getContent(SwaggerReader $reader, array $paths): array
