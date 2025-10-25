95f1dd8acc58c5eddfff7a9de96a67846c90a186
Adrian <freefri@freefri.es>
2025-10-25 16:27:44 +0200

    v0.4.2 (quickfix delete splits)

--- app_rest/src/Controller/SwaggerJsonController.php
+++ app_rest/src/Controller/SwaggerJsonController.php
@@ -10,7 +10,7 @@ class SwaggerJsonController extends \RestApi\Controller\SwaggerJsonController
 {
     public static function version(): string
     {
-        return '0.4.1';
+        return '0.4.2';
     }
 
     protected function getContent(SwaggerReader $reader, array $paths): array
