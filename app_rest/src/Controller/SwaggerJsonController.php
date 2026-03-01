4171612c31a0773de01a6ed3bad1d1caea7e26f9
Adrian <freefri@freefri.es>
2026-03-01 16:08:41 +0100

    v0.4.8 (create ser and reset password flows, refactor front proxy, and migrations)

--- app_rest/src/Controller/SwaggerJsonController.php
+++ app_rest/src/Controller/SwaggerJsonController.php
@@ -10,7 +10,7 @@ class SwaggerJsonController extends \RestApi\Controller\SwaggerJsonController
 {
     public static function version(): string
     {
-        return '0.4.7';
+        return '0.4.8';
     }
 
     protected function getContent(SwaggerReader $reader, array $paths, array $schemas = []): array
